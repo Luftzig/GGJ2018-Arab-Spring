e32259aa612933003e3c380c4e51824caab96c7a
Ariel Grunfeld <arielgrunfeld@gmail.com>
2018-01-27 15:33:11 +0200

    fixed raycasting radius

--- src/RayCasting.elm
+++ src/RayCasting.elm
@@ -148,6 +148,9 @@ rayTracing sunParams source boundaries obstacles =
             Just (radius, nRays) -> createSun radius nRays source
     in
         List.map (\ray -> List.foldr cutRay ray walls) (rawRays ++ extraRays ++ sunRays)
+        |> case sunParams of
+            Nothing -> identity
+            Just (radius, nRays) -> List.map (cutRayAtRadius radius)
 
 
 vectAdd : Vector -> Vector -> Vector
@@ -383,7 +386,7 @@ createSun radius nRays source =
 -----------------------------------------------------------------------------------
 
 maxRadius : Float
-maxRadius = 200
+maxRadius = 100
 
 numberOfSunRays : Int
 numberOfSunRays = 50
